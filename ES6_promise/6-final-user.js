9411b7fe10d1694b62c2994f39524acb8a71586e
ninaglss15 <10526@holbertonstudents.com>
2025-12-19 23:14:15 +0100

    refactor: streamline promise handling in handleProfileSignup function

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,13 +1,9 @@
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
-
+import signUpUser from './4-user-promise.js'
+import uploadPhoto from './5-photo-reject.js'
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const signUpPromise = signUpUser(firstName, lastName);
-  const uploadPromise = uploadPhoto(fileName);
-
-  return Promise.allSettled([signUpPromise, uploadPromise])
-    .then((results) => results.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
-    })));
-}
+  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
+  return Promise.allSettled(promises).then((results) => results.map((result) => ({
+	status: result.status,
+	value: result.status === 'fulfilled' ? result.value : result.reason.toString(),
+  })));
+  }
