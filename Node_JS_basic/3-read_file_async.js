e1c26ce061a3f3ce3b26e4df63569f0789b64c9c
ninaglss15 <10526@holbertonstudents.com>
2026-01-15 09:53:46 +0100

    feat: implement countStudents function to read and process student data from a CSV file

---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+      
+      try {
+        const lines = data.split('\n').filter((line) => line.trim() !== '');
+        const students = lines.slice(1);
+        
+        if (students.length === 0) {
+          reject(new Error('Cannot load the database'));
+          return;
+        }
+        
+        console.log(`Number of students: ${students.length}`);
+        
+        const fields = {};
+        
+        students.forEach((line) => {
+          const [firstname, , , field] = line.split(',');
+          if (firstname && field) {
+            if (!fields[field]) {
+              fields[field] = [];
+            }
+            fields[field].push(firstname);
+          }
+        });
+        
+        Object.keys(fields).forEach((field) => {
+          const studentList = fields[field];
+          console.log(`Number of students in ${field}: ${studentList.length}. List: ${studentList.join(', ')}`);
+        });
+        
+        resolve();
+      } catch (error) {
+        reject(new Error('Cannot load the database'));
+      }
+    });
+  });
+}
+
+module.exports = countStudents;
